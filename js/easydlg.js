4213e57d2301b091e70ff1052318d49f547050fe
keejelo <34620895+keejelo@users.noreply.github.com>
2020-03-13 18:26:50 +0100

    Update easydlg.js

--- js/easydlg.js
+++ js/easydlg.js
@@ -97,8 +97,7 @@ let EasyDialogBox =
 	cancelButtonText : 'Cancel',  // Cancel
 	
 	// ** Dialogbox types, can be used separate or combined
-	strBoxTypeList : ['dlg', 'dlg-close', 'dlg-prompt', 'dlg-yes', 'dlg-no', 'dlg-yes-no', 'dlg-ok', 
-					'dlg-cancel', 'dlg-ok-cancel', 'no-footer', 'no-btns'],
+	strBoxTypeList : ['dlg','dlg-close','dlg-prompt','dlg-yes','dlg-no','dlg-yes-no','dlg-ok','dlg-cancel','dlg-ok-cancel','no-footer','no-btns'],
 
 	// ** Create 'id' for dialogbox, hopefully it won't clash with any other html elements 'id'
 	// ** If we wanted to create an unique 'id' for each dialogbox we could've used a timestamp.
