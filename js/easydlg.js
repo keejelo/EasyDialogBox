9795e2a90dfaa9d3187a00a94d6e99df983339eb
keejelo <34620895+keejelo@users.noreply.github.com>
2020-03-15 15:24:58 +0100

    Update easydlg.js

--- js/easydlg.js
+++ js/easydlg.js
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------------------------------------------------
-// ** EasyDialogBox 1.332
+// ** EasyDialogBox 1.333
 // ** Created by: keejelo, 2020.
 // ** GitHub: https://github.com/keejelo/EasyDialogBox
 //-----------------------------------------------------------------------------------------------------------------
@@ -158,7 +158,7 @@ var EasyDialogBox =
 			
 			// ** Create box and insert into parent element
 			let dlg = document.createElement('div');
-			dlg.setAttribute('id', this.boxId + '_OnTheFly');
+			dlg.setAttribute('id', 'OnTheFly' + this.boxId);
 			dlg.setAttribute('class', strBoxTypeClass);
 			dlg.setAttribute('title', strTitle);
 			dlg.setAttribute('name', strAction);
