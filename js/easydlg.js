121821d40649313039b01ffaf33beb3d64a371b8
keejelo <34620895+keejelo@users.noreply.github.com>
2020-04-05 17:10:43 +0200

    Update easydlg.js

--- js/easydlg.js
+++ js/easydlg.js
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------------------------------------------------
-// ** EasyDialogBox 1.531
+// ** EasyDialogBox 1.532
 // ** Created by: keejelo
 // ** Year: 2020
 // ** GitHub: https://github.com/keejelo/EasyDialogBox
