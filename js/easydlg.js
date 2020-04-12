7dbe5ba4d5e328a9c340ee94500a375c652426c3
keejelo <34620895+keejelo@users.noreply.github.com>
2020-04-12 13:46:43 +0200

    Update easydlg.js

--- js/easydlg.js
+++ js/easydlg.js
@@ -1,6 +1,6 @@
 //-----------------------------------------------------------------------------------------------------------------
 // ** EasyDialogBox
-// ** Version: 1.616
+// ** Version: 1.617
 // ** Created by: keejelo
 // ** Year: 2020
 // ** GitHub: https://github.com/keejelo/EasyDialogBox
